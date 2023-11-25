0a3e220eb9077f62a961e936c4874ac9c8300d54
Anil Kumar <82893113+callmeanil@users.noreply.github.com>
2023-11-26 00:22:56 +0530

    Add files via upload

---
+++ src/routes/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const rootRouter = express.Router();
+
+const users = require('./users_routes/user.route');
+
+
+rootRouter.use('/', users)
+
+module.exports = rootRouter
